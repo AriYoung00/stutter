1826da30d62a2cdb812b846d0a6ee4de788772ac
Ariel Young <ariyoung@pm.me>
2023-04-11 15:23:10 -0700

    rewrote utility in shell for giggles

---
+++ zip_proj.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+if [[ ! -d "$1" ]]; then
+    echo "Could not find directory '$1'"
+    exit 1
+fi
+
+path=$1
+path="${path%/}"
+
+echo "Zipping project at path '$path'"
+
+cd $path
+echo "Cleaning project..."
+cargo clean
+cd -
+
+echo "Zipping project..."
+zip -r "${path}_ayoung.zip" $path
+
+status=$?
+if [ $status -ne 0 ]; then
+    echo "Error zipping project -- code $status"
+    exit 1
+else
+    echo "Successfully zipped project to '${path}_ayoung.zip'"
+    exit 0
+fi
+
