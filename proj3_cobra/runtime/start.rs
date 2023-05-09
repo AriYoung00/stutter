df4836b970712f5eae64152a172c3fa7a8db8791
Ariel Young <ariyoung@pm.me>
2023-05-08 23:00:32 -0700

    Update start.rs

--- proj3_cobra/runtime/start.rs
+++ proj3_cobra/runtime/start.rs
@@ -6,7 +6,7 @@ extern "C" {
     // it does not add an underscore in front of the name.
     // Courtesy of Max New (https://maxsnew.com/teaching/eecs-483-fa22/hw_adder_assignment.html)
     #[link_name = "\x01our_code_starts_here"]
-    fn our_code_starts_here(input: i64) -> u64;
+    fn our_code_starts_here(input: i64) -> i64;
 }
 
 #[export_name = "\x01snek_error"]
@@ -16,6 +16,7 @@ pub extern "C" fn snek_error(errcode: i64) {
         7 => "invalid argument - expected number, not bool",
         8 => "invalid argument - expected bool, not number",
         9 => "error - arithmetic overflow",
+        10 => "invalid argument(s) - mismatched operand types",
         _ => "an unknown error occurred",
     });
     std::process::exit(1);
@@ -24,21 +25,17 @@ pub extern "C" fn snek_error(errcode: i64) {
 fn parse_input(input: &str) -> i64 {
     // TODO: parse the input string into internal value representation
     if input == "true" {
-        0b11
+        3 // 0b11 = true
     }
     else if input == "false" {
-        0b01
+        1 // 0b01 = false
     }
     else {
         let val: i64 = input.parse().expect("invalid argument");
         if val > 4611686018427387903 || val < -4611686018427387904 {
             panic!("invalid argument")
         }
-
-        let preserve_sign_mask = val & (1 << 63);
-        let shifted = val << 1;
-
-        shifted | preserve_sign_mask
+        val << 1
     }
 }
 
@@ -52,7 +49,7 @@ fn main() {
     let input = if args.len() == 2 { &args[1] } else { "false" };
     let input = parse_input(&input);
 
-    let i: u64 = unsafe { our_code_starts_here(input) };
+    let i: i64 = unsafe { our_code_starts_here(input) };
     if i == 3 {
         println!("true");
     }
