79c5b17eaf0d0e73050dad05dbe35864531eb891
Ariel Young <ariyoung@pm.me>
2023-05-11 01:59:27 -0700

    added SNEK_PRINT label

--- compiler/src/util.rs
+++ compiler/src/util.rs
@@ -3,4 +3,6 @@ pub const EXIT_EXPECTED_NUM: &str = "exit_expected_number";
 pub const EXIT_EXPECTED_BOOL: &str = "exit_expected_bool";
 pub const EXIT_OVERFLOW: &str = "exit_overflow";
 
+pub const SNEK_PRINT: &str = "snek_print";
+
 pub type ParseResult<T> = Result<T, String>;
